f155258c3c273c1c4d7a2ead16a45c02894e54bc
tax2494 <s_guy_malick@hotmail.com>
2025-12-13 10:12:13 -0600

    add plaid+supabase libs

---
+++ lib/plaid.ts
@@ -0,0 +1,12 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+
+const config = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID!,
+      "PLAID-SECRET": process.env.PLAID_SECRET_SANDBOX!,
+    },
+  },
+});
+export const plaidClient = new PlaidApi(config);
